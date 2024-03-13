bc2e9fe987f68a62f85099aaec363cb81482e355
dhavalnerds97 <dhaval.prajapati@nerdshouse.com>
2024-03-13 18:12:52 +0530

    added headless wordpress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
@@ -10,6 +10,8 @@ export default defineConfig({
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+      domains: ["astro.build", "https://letusguide.com/"],
+      remotePatterns: [{ protocol: "https" }],
     }),
     mdx(),
     sitemap(),
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+WORDPRESS_API_URL =  "http://localhost/headless-wordpress/graphql"
